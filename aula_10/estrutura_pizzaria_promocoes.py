5d0475e75d62b51aad5eb2d6cadb588bc69e5558
Marco <marcorochan@outlook.com>
2026-03-27 09:45:39 -0300

    alteracoes_27_03_26

---
+++ aula_10/estrutura_pizzaria_promocoes.py
@@ -0,0 +1,26 @@
+# Variáveis da Pizzaria
+FRETE = 2 #Constante Fake
+pizza_sabor = input("Informe o sabor da pizza - [frango com requeijão], [calabresa], [mussarela], [banana com chocolate]: ")
+pizza_tamanho = input("Informe o tamanho da pizza - [pequena], [média], [grande]: ")
+dia_semana = input("Informe o dia da semana - [quarta], [quinta], [sexta], [sábado], [domingo]: ")
+
+print(f"O sabor escolhido é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}.")
+
+# Promoções -> Estruturas condicionais
+
+# Comprando qualquer pizza e qualquer tamanho no sábado,
+# o refri é gratuito.
+if dia_semana == "sábado":
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O refri é por conta da casa.")
+elif dia_semana == "domingo":
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O refri e o frete são por conta da casa.")
+elif pizza_sabor == "calabresa" and pizza_tamanho == "média":
+    print(f"🍕 Pedido aceito com sucesso!")
+    print(f"O frete é por conta da casa.")
+
+
+#Comprando uma pizza de calabresa tamanho médio, em qualquer dia, o frete é grauito.
+
+#Comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos.
